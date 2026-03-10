bf639bf41d277e2ed1f626ea6bcaddbaf8c3e22a
miradojonas <jonas.ranaivohasivony@esti.mg>
2026-03-10 22:34:37 +0300

    CI fail again

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,10 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        ]);
-
         $middleware->throttleApi('api');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
